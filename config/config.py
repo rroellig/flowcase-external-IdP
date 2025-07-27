b5c391b8e35fb9c6cabb137923fe98e424c69e90
René Röllig <rene.roellig@mailbox.org>
2025-07-27 22:29:19 +0200

    gunicorn wsgi integrated.

--- config/config.py
+++ config/config.py
@@ -2,6 +2,10 @@ import os
 import argparse
 import random
 import string
+from sqlalchemy.dialects import registry
+
+# Register our custom SQLite dialect with WAL mode
+registry.register("sqlite.pysqlite_wal", "utils.sqlite_wal", "SQLiteDialect_pysqlite_wal")
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -13,7 +17,21 @@ def parse_args():
 	return parser.parse_known_args()[0]
 
 def configure_app(app, config=None):
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), 'data', 'flowcase.db')
+	# Configure SQLite with WAL journal mode and connection pooling to avoid locking issues with Gunicorn
+	sqlite_path = os.path.join(os.getcwd(), 'data', 'flowcase.db')
+	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite+pysqlite_wal:///{sqlite_path}'
+	
+	# Configure SQLAlchemy engine options for better concurrency with SQLite
+	app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+		'connect_args': {
+			'timeout': 60,  # 60 second timeout
+			'check_same_thread': False,  # Allow threads to share the connection
+		},
+		'pool_size': 10,  # Maximum number of connections to keep
+		'max_overflow': 20,  # Maximum number of connections that can be created beyond pool_size
+		'pool_timeout': 30,  # Seconds to wait before giving up on getting a connection from the pool
+		'pool_recycle': 1800,  # Recycle connections after 30 minutes
+	}
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 	
 	os.makedirs("data", exist_ok=True)
